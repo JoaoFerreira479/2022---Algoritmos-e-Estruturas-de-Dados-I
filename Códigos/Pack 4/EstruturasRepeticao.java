f0c467b1c96490f5df5a713a5a3d471fb0a9f4ee
JoaoFerreira479 <164263649+JoaoFerreira479@users.noreply.github.com>
2025-01-05 16:05:21 -0300

    Update EstruturasRepeticao.java

--- Códigos/Pack 4/EstruturasRepeticao.java
+++ Códigos/Pack 4/EstruturasRepeticao.java
@@ -1,26 +1,20 @@
-package estruturasrepeticao;
-
 public class EstruturasRepeticao {
 
     public static void main(String[] args) {
         System.out.println("Imprimindo os números de 5 a 15 usando diferentes estruturas de repetição:");
 
-        // Usando while
         System.out.println("\nUsando while:");
         imprimirComWhile(5, 15);
 
-        // Usando do-while
         System.out.println("\nUsando do-while:");
         imprimirComDoWhile(5, 15);
 
-        // Usando for
         System.out.println("\nUsando for:");
         imprimirComFor(5, 15);
 
-        System.out.println(); // Quebra de linha final
+        System.out.println();
     }
 
-    // Método para imprimir números usando while
     private static void imprimirComWhile(int inicio, int fim) {
         int i = inicio;
         while (i <= fim) {
@@ -29,7 +23,6 @@ public class EstruturasRepeticao {
         }
     }
 
-    // Método para imprimir números usando do-while
     private static void imprimirComDoWhile(int inicio, int fim) {
         int i = inicio;
         do {
@@ -38,7 +31,6 @@ public class EstruturasRepeticao {
         } while (i <= fim);
     }
 
-    // Método para imprimir números usando for
     private static void imprimirComFor(int inicio, int fim) {
         for (int i = inicio; i <= fim; i++) {
             System.out.print(i + " ");
